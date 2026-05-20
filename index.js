e25a1c584a98c5bb530fbfdc2da4b4ea78c187a6
Arjay18 <arjaydomingo008@gmail.com>
2026-05-20 10:29:12 +0800

    nsdkj

--- index.js
+++
@@ -1,6 +0,0 @@
-// This file acts as the entry point for Vercel Serverless Functions
-// It imports the Express app from your server folder and exports it
-
-module.exports = (req, res) => {
-  res.status(200).send('Hello from Vercel API!');
-};
