aff93e8e21bca829f2aaa166ef88c411dd06e958
Arjay18 <arjaydomingo008@gmail.com>
2026-05-20 09:50:10 +0800

    new fix build

--- node_modules/client/index.js
+++
@@ -1,5 +0,0 @@
-// This file acts as the entry point for Vercel Serverless Functions
-// It imports the Express app from your server folder and exports it
-const app = require('../server/index.js');
-
-module.exports = app;
