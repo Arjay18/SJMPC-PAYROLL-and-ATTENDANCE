52f711aa15f827a82080c494f437a1c31f7f6345
Arjay18 <arjaydomingo008@gmail.com>
2026-05-20 16:01:20 +0800

    igjhg

--- node_modules/client/src/auth/api.js
+++ node_modules/client/src/auth/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
+  baseURL: '/api',
 })
+
 
 export function setAuthToken(token){
   if(token) api.defaults.headers.common['Authorization'] = `Bearer ${token}`
