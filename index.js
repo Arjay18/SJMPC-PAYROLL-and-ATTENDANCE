4e7de58a687e9a052008160201edd76e63763102
Arjay18 <arjaydomingo008@gmail.com>
2026-05-20 10:07:29 +0800

    nm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This file acts as the entry point for Vercel Serverless Functions
 // It imports the Express app from your server folder and exports it
 require('dotenv').config();
-const app = require('../server/index.js');
+const app = require('./server/index.js');
 
 module.exports = app;
